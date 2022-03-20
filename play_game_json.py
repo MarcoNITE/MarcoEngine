aed662cea3977e9c5d33be89ddcbf35012522f37
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-03-20 11:18:59 +0300

    Added comments in game json playing

--- play_game_json.py
+++ play_game_json.py
@@ -1,29 +1,30 @@
-import json
-import sys
+import json # json dictionaries
+import sys # system
 
-import chess
+import chess # chess
 
 
 def play(filename):
-    if filename.endswith('.json'):
-        game = json.load(open(filename, 'r'))
+    """Playing a game"""
+    if filename.endswith('.json'): # if file is ended with .json...
+        game = json.load(open(filename, 'r')) # loading game
 
-    else:
-        game = json.load(open('r', filename + '.json'))
+    else: # else
+        game = json.load(open('r', filename + '.json')) # loading game
 
-    game_keys = game.keys()
+    game_keys = game.keys() # game moves
 
-    board = chess.Board()
+    board = chess.Board() # chess board
 
-    try:
-        for key in game_keys:
-            board = chess.Board(str(game[key]))
-            print(board.unicode())
-            print(key)
-            print('\n' * 3)
+    try: # trying
+        for key in game_keys: # cycle of moves
+            board = chess.Board(str(game[key])) # sets board with keys
+            print(board.unicode()) # printing board with unicode
+            print(key) # printing move 
+            print('\n' * 3) # enters
 
-    finally:
-        print()
+    finally: # and finally...
+        print() # enter
 
 
-play(sys.argv[1])
+play(sys.argv[1]) # plays a game on args of game
