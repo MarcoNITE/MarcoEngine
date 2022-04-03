40784891af51716f14561463a4134b09490733f1
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-03 10:37:21 +0300

    Add new tests for PyTest in tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,26 @@
+import python_checking
 import pytest
-
+import requests
+import shutil
+import zipfile
+import os
 
 def test_nothing():
     assert True
 
+def check_os():
+    python_checking.check()
+
+def download_me():
+    response = requests.get("https://github.com/MarcoNITE/MarcoEngine/archive/master.zip", allow_redirects=True)
+    with open("master.zip", "wb") as file:
+        file.write(response.content)
+    with zipfile.ZipFile("master.zip", "r") as zip_ref:
+        zip_ref.extractall(".")
+
+def delete_downloaded_zip():
+    os.remove("master.zip")
+
+check_os()
+download_me()
+delete_downloaded_zip()
