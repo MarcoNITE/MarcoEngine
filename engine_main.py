22b7726ed289cdb0ee365bd40b96d0b437ea97e5
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-05 07:38:17 +0300

    Add engine file

---
+++ engine_main.py
@@ -0,0 +1,51 @@
+import train # training agent
+import chess # chess
+import time # time module
+
+def go(command: str, engine, board: chess.Board,
+        depth: int = 1000,
+        movetime: int = -1):
+        """Analyzing position"""
+
+        # parse values
+        _, *params = command.split(' ')  # parameters
+        for param, val in zip(*2 * (iter(params),)):  # cycle of params and values
+            if param == 'depth':  # depth param
+                depth = int(val)  # depth value
+            if param == 'movetime':  # movetime param
+                movetime = int(val)  # movetime value
+            if param == 'wtime':  # wtime param
+                our_time = int(val)  # wtime value
+            if param == 'btime':  # btime param
+                opp_time = int(val)  # btime value
+
+        moves_remain = 40  # remaining moves
+
+        start = time.time()  # start time
+        ponder = None  # ponder move
+        for sdepth in range(0, depth + 1):  # cycle of self-depth and depth
+
+            if True:  # if showing thinking
+                if board.turn:  # if white to move
+                    score = train.analyze(engine, board, sdepth)  # score
+                else:  # if black to move
+                    score = train.analyze(engine, board, sdepth)  # score
+
+                usedtime = int((time.time() - start) * 1000)  # time used
+                print(
+                    'info depth {} score cp {} time {}'.format(sdepth, score, usedtime))  # about thinking
+
+            for m in range(0, 2):  # generate moves. 2 for ponder
+                best_move = train.best_move(engine, board, sdepth)  # append moves to moves list
+                print("bestmove " + str(best_move))  # send response about best move
+
+            # checks for no-bugs
+            if movetime > 0 and (time.time() - start) * 1000 > movetime:
+                break
+
+            if sdepth >= depth:
+                break
+
+        print("bestmove " + str(best_move))
+
+        del best_move  # delete best move
