13395bc977e07435530576a0a1cfed74e541ad5e
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-09 11:28:43 +0300

    Fix training test

--- tests/train_test.py
+++ tests/train_test.py
@@ -3,7 +3,7 @@ import chess # chess
 import chess.engine # chess engine
 
 def test_train():
-	"""Testing training"""
+    """Testing training"""
     test_board = chess.Board() # tests board
     _engine = chess.engine.SimpleEngine.popen_uci("./TEMP/sf") # openning engine
     train.train() # start training
