b4fd5ac9270bb683e31036cd10cd55cbf220fcc0
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-09 14:30:11 +0300

    Add stockfish downloader

---
+++ makefish.sh
@@ -0,0 +1,4 @@
+wget https://stockfishchess.org/files/stockfish_14.1_linux_x64.zip
+unzip stockfish_14.1_linux_x64.zip
+mv stockfish_14.1_linux_x64/stockfish_14.1_linux_x64 stockfish
+chmod +x stockfish
