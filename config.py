8cc9cedfdc3640c8ea25486ad633b7686236db9c
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-09 14:30:34 +0300

    Fix stockfish name in config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 SHOW_THINKING = True # showing thinking in generating best move?
-ENGINE_DIR = "stockfish" # engine directory
+ENGINE_DIR = "./stockfish" # engine directory
 DEFAULT_DEPTH = 10 # default depth
 TRAINING_DEPTH = 10 # depth for training
 BLACK_SCORE = -40 # score of board(for black) to say: error
