3550726dd9c9ed459e3b957525d39ad3f9b145b8
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-09 11:34:12 +0300

    Update engine name in training test

--- train_test.py
+++ train_test.py
@@ -6,6 +6,6 @@ def test_train():
     """Testing training"""
     test_board = chess.Board() # tests board
     _engine = chess.engine.SimpleEngine.popen_uci("./TEMP/sf") # openning engine
-    train.train(engine=engine, board=test_board) # start training
+    train.train(engine=_engine, board=test_board) # start training
 
 test_train() # starting!
