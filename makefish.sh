09d055015f252cec5e04b43ec22bddd59b7e96ed
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-09 14:41:43 +0300

    Add auto directory deleting after download

--- makefish.sh
+++ makefish.sh
@@ -2,3 +2,5 @@ wget https://stockfishchess.org/files/stockfish_14.1_linux_x64.zip
 unzip stockfish_14.1_linux_x64.zip
 mv stockfish_14.1_linux_x64/stockfish_14.1_linux_x64 stockfish
 chmod +x stockfish
+rm stockfish_14.1_linux_x64/*
+rmdir stockfish_14.1_linux_x64
