93326291ef632f4de0d153c5359343b871e4e4a6
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-09 11:33:13 +0300

    Update training test

--- train_test.py
+++ train_test.py
@@ -6,6 +6,6 @@ def test_train():
     """Testing training"""
     test_board = chess.Board() # tests board
     _engine = chess.engine.SimpleEngine.popen_uci("./TEMP/sf") # openning engine
-    train.train() # start training
+    train.train(engine=engine, board=test_board) # start training
 
 test_train() # starting!
