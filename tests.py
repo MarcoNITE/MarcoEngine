4490148286206234b1e68a4a92cac0a1c788058f
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-09 09:06:02 +0300

    Add new tests to PyTest

--- tests.py
+++ tests.py
@@ -1,15 +1,28 @@
 import python_checking
 import pytest
 import requests
-import shutil
 import zipfile
 import os
+import sys
+import shutil
+import stat
+import chess
+import chess.engine
+import train
+
+platform = sys.platform
+file_extension = ".exe" if platform == "win32" else ""
 
 def test_nothing():
     assert True
 
 def check_os():
-    python_checking.check()
+    if platform == "linux":
+        assert True
+        return
+
+    else:
+        assert False
 
 def download_me():
     response = requests.get("https://github.com/MarcoNITE/MarcoEngine/archive/master.zip", allow_redirects=True)
@@ -21,6 +34,28 @@ def download_me():
 def delete_downloaded_zip():
     os.remove("master.zip")
 
+def download_sf():
+    windows_or_linux = "win" if platform == "win32" else "linux"
+    response = requests.get(f"https://stockfishchess.org/files/stockfish_14.1_{windows_or_linux}_x64.zip", allow_redirects=True)
+    with open("./TEMP/sf_zip.zip", "wb") as file:
+        file.write(response.content)
+    with zipfile.ZipFile("./TEMP/sf_zip.zip", "r") as zip_ref:
+        zip_ref.extractall("./TEMP/")
+    shutil.copyfile(f"./TEMP/stockfish_14.1_{windows_or_linux}_x64/stockfish_14.1_{windows_or_linux}_x64{file_extension}", f"./TEMP/sf{file_extension}")
+    shutil.copyfile(f"./TEMP/sf{file_extension}", f"./TEMP/sf2{file_extension}")
+    if windows_or_linux == "linux":
+        st = os.stat(f"./TEMP/sf{file_extension}")
+        os.chmod(f"./TEMP/sf{file_extension}", st.st_mode | stat.S_IEXEC)
+        st = os.stat(f"./TEMP/sf2{file_extension}")
+        os.chmod(f"./TEMP/sf2{file_extension}", st.st_mode | stat.S_IEXEC)
+
+def test_train():
+    test_board = chess.Board()
+    _engine = chess.engine.SimpleEngine.popen_uci("./TEMP/sf") # openning engine
+    train.train()
+
 check_os()
 download_me()
 delete_downloaded_zip()
+download_sf()
+
