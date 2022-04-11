123bf34f3b8c70c313dd2b7a0707f8db1d650361
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-11 20:00:18 +0300

    Create train_test.py

---
+++ train_test.py
@@ -0,0 +1,9 @@
+import chess # chess
+import chess.engine # chess engine
+import train # Training agent
+
+test_board = chess.Board() # our testing board
+_engine = chess.engine.SimpleEngine.popen_uci("./stockfish") # engine
+
+train.train(engine=_engune, board=test_board) # starting train
+_engine.quit() # quiting from engine
