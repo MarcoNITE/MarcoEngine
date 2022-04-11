3bf4fd94458f62d6e26f67c70df2c948203ed890
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-11 10:43:06 +0300

    Fix engine name in train_test.py

--- train_test.py
+++ train_test.py
@@ -5,7 +5,7 @@ import chess.engine # chess engine
 def test_train():
     """Testing training"""
     test_board = chess.Board() # tests board
-    _engine = chess.engine.SimpleEngine.popen_uci("./TEMP/sf") # openning engine
+    _engine = chess.engine.SimpleEngine.popen_uci("./stockfish") # openning engine
     train.train(engine=_engine, board=test_board) # start training
 
 test_train() # starting!
