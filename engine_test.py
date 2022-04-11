f58227e17b015b10a4eadb56f5d0b917cc50f06b
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-04-11 10:54:24 +0300

    Fix engine argument in engine_test.py

--- engine_test.py
+++ engine_test.py
@@ -6,6 +6,6 @@ def test_move():
     """Testing training"""
     test_board = chess.Board() # tests board
     _engine = chess.engine.SimpleEngine.popen_uci("./stockfish") # openning engine
-    engine_main.go(command="go depth 20", engine=_engine, board=test_board) # start training
+    engine_main.go(command="go depth 20", _engine=_engine, board=test_board) # start training
 
 test_move() # starting!
