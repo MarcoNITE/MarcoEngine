5a48e266400706bb608ed51ad7495581a7fc71a2
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-05-31 17:47:01 +0300

    Fix learning config

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ ENGINE_DIR = "./stockfish" # engine directory
 DEFAULT_DEPTH = 10 # default depth
 TRAINING_DEPTH = 10 # depth for training
 BLACK_SCORE = -40 # score of board(for black) to say: error
-WHITE_SCORE = 40 # score of board(for black) to say: error
+WHITE_SCORE = 50 # score of board(for black) to say: error
