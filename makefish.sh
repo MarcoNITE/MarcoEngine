dcd2655ad1891054f4444bb87dd4d742fb7de9e6
AB in MT <96744017+AB-MT@users.noreply.github.com>
2022-06-27 22:48:15 +0300

    Fix not work build

--- makefish.sh
+++ makefish.sh
@@ -2,5 +2,5 @@ wget https://stockfishchess.org/files/stockfish_15_linux_x64.zip
 unzip stockfish_15_linux_x64.zip
 mv stockfish_15_linux_x64/stockfish_15_linux_x64 stockfish
 chmod +x stockfish
-rm stockfish_14.1_linux_x64.zip
+rm stockfish_15_linux_x64.zip
 printf Enjoy! 
