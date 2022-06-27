8ae3b9c39daf1a284ba95cbd0b28d47b67fee43f
AB in MT <96744017+AB-MT@users.noreply.github.com>
2022-06-27 22:47:05 +0300

    Fix Stockfish version to Stockfish 15 linux x64

--- makefish.sh
+++ makefish.sh
@@ -1,6 +1,6 @@
-wget https://stockfishchess.org/files/stockfish_14.1_linux_x64.zip
-unzip stockfish_14.1_linux_x64.zip
-mv stockfish_14.1_linux_x64/stockfish_14.1_linux_x64 stockfish
+wget https://stockfishchess.org/files/stockfish_15_linux_x64.zip
+unzip stockfish_15_linux_x64.zip
+mv stockfish_15_linux_x64/stockfish_15_linux_x64 stockfish
 chmod +x stockfish
 rm stockfish_14.1_linux_x64.zip
 printf Enjoy! 
