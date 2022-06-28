a5f830946ed6cb4d24602ddf94358daab8fc3a58
AB in MT <96744017+AB-MT@users.noreply.github.com>
2022-06-28 07:12:12 +0300

    Full fix of Stockfish Making

--- makefish.sh
+++ makefish.sh
@@ -1,6 +1,7 @@
 wget https://stockfishchess.org/files/stockfish_15_linux_x64.zip
 unzip stockfish_15_linux_x64.zip
-mv stockfish_15_linux_x64/stockfish_15_linux_x64 stockfish
-chmod +x stockfish
+mv stockfish_15_linux_x64/ stockfish_15_x64
+chmod +x stockfish_15_x64
 rm stockfish_15_linux_x64.zip
+mv stockfish_15_linux_x64 stockfish
 printf Enjoy! 
